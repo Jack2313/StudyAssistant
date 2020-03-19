a7e38f125b3b6239307fb767913400a34a35e79f
Jack2313 <570055398@qq.com>
2020-03-19 23:11:27 +0800

    first commit

---
+++ src/main/java/com/komnenos/studyassistant/StudyassistantApplication.java
@@ -0,0 +1,13 @@
+package com.komnenos.studyassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyassistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyassistantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/komnenos/studyassistant/StudyassistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.komnenos.studyassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyassistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
